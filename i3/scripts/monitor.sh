84df01635e44212e7d17efd2ceb5fc360d783824
Arnold Vásquez <contacto@disroot.org>
2023-03-30 09:30:02 -0300

    polkit agent added

--- i3/scripts/monitor.sh
+++ i3/scripts/monitor.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/usr/bin/env bash
 if [[ $(xrandr -q | grep "HDMI-0 connected") ]]; then
 xrandr --output eDP --primary --mode 1366x768 --pos 0x0 --rotate normal --output HDMI-0 --mode 1920x1080 --pos 1366x0 --rotate normal --output VGA-0 --off
 fi
