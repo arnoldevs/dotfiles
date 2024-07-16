957b5949c3ee049b69366962859dea61a26bee79
Arnold Vasquez <contacto@disroot.org>
2024-07-16 14:01:44 -0400

    pre os change

--- bashrc.d/env.sh
+++ bashrc.d/env.sh
@@ -2,11 +2,11 @@
 
 # User specific aliases and functions
 if [ -d ~/.bashrc.d ]; then
-  for rc in ~/.bashrc.d/*; do
-    if [ -f "$rc" ]; then
-      . "$rc"
-    fi
-  done
+	for rc in ~/.bashrc.d/*; do
+		if [ -f "$rc" ]; then
+			. "$rc"
+		fi
+	done
 fi
 unset rc
 
@@ -15,16 +15,16 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion" # This loads nvm bash_completion
 
 if type -P javac >/dev/null; then
-  export JAVA_HOME="/usr/lib/jvm/default-java"
-  export PATH="$JAVA_HOME/bin:$PATH"
+	export JAVA_HOME="/usr/lib/jvm/default-java"
+	export PATH="$JAVA_HOME/bin:$PATH"
 fi
 
 [[ -d "/opt/nvim-linux64" ]] && export PATH="$PATH:/opt/nvim-linux64/bin"
 
 if [[ -d "$HOME/.cargo" ]]; then
-  . "$HOME/.cargo/env"
-  source <(rustup completions bash)       # for rustup
-  source <(rustup completions bash cargo) # for cargo
+	. "$HOME/.cargo/env"
+	source <(rustup completions bash)       # for rustup
+	source <(rustup completions bash cargo) # for cargo
 fi
 
 [[ "$(readlink /proc/$$/exe)" = "/usr/bin/bash" ]] && bind 'set completion-ignore-case on'
@@ -40,10 +40,12 @@ if type -P atuin >/dev/null; then eval "$(atuin init bash)"; fi
 if type -P fzf >/dev/null; then source /usr/share/doc/fzf/examples/key-bindings.bash; fi
 
 if type -P go >/dev/null; then
-  [[ -d /usr/local/go ]] && export PATH=$PATH:/usr/local/go/bin
-  export GOPATH=$HOME/.go
-  export GOBIN=$GOPATH/bin
-  export PATH=$PATH:$GOBIN
+	[[ -d /usr/local/go ]] && export PATH=$PATH:/usr/local/go/bin
+	export GOPATH=$HOME/.go
+	export GOBIN=$GOPATH/bin
+	export PATH=$PATH:$GOBIN
 fi
+
+export PATH=$PATH:/usr/sbin
 
 [[ -d "$HOME/.dotfiles" ]] && . "$HOME/.dotfiles/bashrc.d/mu.sh"
---
+++ wezterm/wezterm.lua
@@ -0,0 +1,54 @@
+local wezterm = require("wezterm")
+
+local config = {}
+
+config.enable_scroll_bar = true
+config.min_scroll_bar_height = "1cell"
+config.font = wezterm.font("JetBrainsMono Nerd Font")
+config.audible_bell = "SystemBeep"
+config.window_background_opacity = 0.95
+config.warn_about_missing_glyphs = false
+config.window_decorations = "INTEGRATED_BUTTONS|RESIZE"
+config.integrated_title_button_style = "Gnome"
+config.hide_tab_bar_if_only_one_tab = true
+
+function get_appearance()
+	if wezterm.gui then
+		return wezterm.gui.get_appearance()
+	end
+	return "Dark"
+end
+
+function scheme_for_appearance(appearance)
+	if appearance:find("Dark") then
+		config.colors = {
+			scrollbar_thumb = "504945",
+		}
+		config.window_frame = {
+			active_titlebar_bg = "#282828",
+			inactive_titlebar_bg = "282828",
+		}
+		return "GruvboxDark"
+	else
+		config.colors = {
+			scrollbar_thumb = "#d5c4a1",
+		}
+		config.window_frame = {
+			active_titlebar_bg = "#fbf1c7",
+			inactive_titlebar_bg = "#fbf1c7",
+		}
+		return "GruvboxLight"
+	end
+end
+
+config.color_scheme = scheme_for_appearance(get_appearance())
+
+config.mouse_bindings = {
+	{
+		event = { Up = { streak = 1, button = "Right" } },
+		mods = "NONE",
+		action = wezterm.action({ PasteFrom = "PrimarySelection" }),
+	},
+}
+
+return config
